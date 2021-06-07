6342410b1d9fbc926dc09aeeb8d36a0c06665eac
guyihan-zb <2092332348@qq.com>
2021-06-07 12:50:53 +0800

    快到碗里来

---
+++ every question/20210517 (2).java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            Double n = sc.nextDouble();
+            Double c = sc.nextDouble();
+            if(n<=2*3.14*c){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+    }
+}
