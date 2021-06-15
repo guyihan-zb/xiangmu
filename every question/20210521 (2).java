6fb3ba958d65dc95ead19d40fbf8665b7eeb3c8e
guyihan-zb <2092332348@qq.com>
2021-06-15 19:49:27 +0800

    因子个数

---
+++ every question/20210521 (2).java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            int n = input.nextInt();
+            if (isPrimer(n)) {
+                System.out.println(1);
+                continue;
+            }
+            int cnt = 0;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    while (n % i == 0) {
+                        n /= i;
+                    }
+                    cnt++;
+                }
+            }
+            if (n != 1) cnt++;
+            System.out.println(cnt);
+        }
+    }
+ 
+    public static boolean isPrimer(int n) {
+        for (int i = 2; i <= Math.pow(n, 0.5); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
