736e1b227834c588fe8711984c8116e7c9b784a5
guyihan-zb <2092332348@qq.com>
2021-06-27 23:03:22 +0800

    spring

---
+++ demo/demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+/**
+ * [description]
+ *
+ * @author： zhangbo
+ * @date: 2021-06-27
+ * @time: 23:02
+ */
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String password;
+    private String img;
+}
