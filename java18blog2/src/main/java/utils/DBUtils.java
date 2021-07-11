3143907f87f98c0ffb8ba0a621bfbd4039e97117
guyihan-zb <2092332348@qq.com>
2021-07-11 23:11:01 +0800

    修改URL，连接sql

--- java18blog2/src/main/java/utils/DBUtils.java
+++ java18blog2/src/main/java/utils/DBUtils.java
@@ -26,7 +26,7 @@ public class DBUtils {
         if (dataSource == null) {
             // 第一次调用
             dataSource = new MysqlDataSource();
-            dataSource.setURL("jdbc:mysql://localhost:3306/java18blog?serverTimezone=GMT%2B8");
+            dataSource.setURL("jdbc:mysql://localhost:3306/java18blog?characterEncoding=utf8&useSSL=false");
             dataSource.setUser("root");
             // [设置你自己的密码]
             //dataSource.setPassword("22458hijk");
