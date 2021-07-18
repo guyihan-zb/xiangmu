ad1c75183b2acc1e097bb2cc0b3f2ceef2aedc9b
guyihan-zb <2092332348@qq.com>
2021-07-18 23:27:31 +0800

    二维数组中的查找

--- every question/20210620.java
+++ every question/20210620.java
@@ -15,3 +15,30 @@ public class Solution {
         return false;
     }
 }
+
+//第二种
+public class Solution {
+    public boolean Find(int target, int [][] array) {
+        int row = array.length;
+        int col = array[0].length;
+        if(row==0){
+            return false;
+        }
+        if(col==0){
+            return false;
+        }
+        //左下
+        int row1 = row -1;
+        int col1 = 0;
+        while(col > col1 && row1 >= 0){
+            if(array[row1][col1] < target){
+                col1++;
+            }else if(array[row1][col1] > target){
+                row1--;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
