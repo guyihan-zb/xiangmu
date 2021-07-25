7048bb9e2722ed7373142c824b351e8fe661f9c4
guyihan-zb <2092332348@qq.com>
2021-07-25 21:54:36 +0800

    矩形覆盖

---
+++ every question/20210719.java
@@ -0,0 +1,17 @@
+//矩形覆盖
+
+
+public class Solution {
+    public int rectCover(int target) {
+        if(target==0||target==1||target==2){
+            return target;
+        }
+        int a = 1,b = 2 ,c = 0;
+        for(int i=3;i<=target;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
