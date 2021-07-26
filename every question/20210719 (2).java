0db25783cd7a6f06817f88f80cef471efc3c972e
guyihan-zb <2092332348@qq.com>
2021-07-26 23:20:32 +0800

    二进制中1的个数

---
+++ every question/20210719 (2).java
@@ -0,0 +1,13 @@
+//二进制中1的个数
+
+
+public class Solution {
+    public int NumberOf1(int n) {
+        int count = 0;
+        while(n!=0){
+            count++;
+            n=n&(n-1);
+        }
+        return count;
+    }
+}
