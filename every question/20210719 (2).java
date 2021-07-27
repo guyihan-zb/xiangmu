c405396c030faf25642e802c9d069c3d0c2225bb
guyihan-zb <2092332348@qq.com>
2021-07-27 22:24:45 +0800

    二进制中1的个数

--- every question/20210719 (2).java
+++ every question/20210719 (2).java
@@ -11,3 +11,26 @@ public class Solution {
         return count;
     }
 }
+
+
+
+1、先将数字转换成二进制字符串
+2、用String.split()函数存入一个数组中
+3、遍历数组跟1比较，同时计数
+4、输出计数值
+
+
+public class Solution {
+    public int NumberOf1(int n) {
+        String s=Integer.toBinaryString(n);
+        String[] split=s.split("");
+        int a=0;
+        for(int i = 0; i < split.length; i++) {
+            if (split[i].equals("1"))
+                {
+                    a++;
+                }
+        }
+             return a;
+    }
+}
