1c60336b2befe356b71eba7e33b7c5ecd6a47e2c
guyihan-zb <2092332348@qq.com>
2021-07-28 22:53:45 +0800

    字符串中找出连续最长的数字串

---
+++ every question/20210719ceshi.java
@@ -0,0 +1,21 @@
+字符串中找出连续最长的数字串
+
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	while (sc.hasNext()) {
+		String str=sc.nextLine();
+		int max=0,end=0,count=0;
+		for (int i = 0; i < str.length(); i++) {
+			if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+				count++;
+				if(max<count){
+					max=count;
+					end=i;
+				}
+			}else{
+				count=0;
+			}
+		}
+		System.out.println(str.substring(end-max+1,end+1));
+	}
+}
