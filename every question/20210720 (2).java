679d764d0f5a7984b8f10f15655120b1c2a10938
guyihan-zb <2092332348@qq.com>
2021-08-05 23:07:53 +0800

    调整数组顺序使奇数位于偶数前面

--- every question/20210720 (2).java
+++ every question/20210720 (2).java
@@ -1,5 +1,6 @@
-import java.util.*;
 
+//调整数组顺序使奇数位于偶数前面
+import java.util.*;
 
 public class Solution {
     /**
@@ -28,6 +29,41 @@ public class Solution {
                 }
                 array[k] = temp;
                 j++;
+            }
+        }
+        return array;
+    }
+}
+
+
+import java.util.*;
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param array int整型一维数组 
+     * @return int整型一维数组
+     */
+    public int[] reOrderArray (int[] array) {
+        // write code here
+        if(array.length == 0){
+            return array;
+        }
+        Queue<Integer> jishu = new LinkedList<>();
+        Queue<Integer> oushu = new LinkedList<>();
+        for(int i=0;i<array.length;i++){
+            if(array[i]%2==0){
+                oushu.add(array[i]);
+            }else{
+                jishu.add(array[i]);
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            if(!jishu.isEmpty()){
+                array[i] = jishu.poll();
+            }else{
+                array[i] = oushu.poll();
             }
         }
         return array;
