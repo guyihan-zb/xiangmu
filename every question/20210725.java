2bc3483570a470ab607c8e3d6141d4977af732bd
guyihan-zb <2092332348@qq.com>
2021-08-14 22:02:18 +0800

    二叉搜索树与双向链表

---
+++ every question/20210725.java
@@ -0,0 +1,27 @@
+//二叉搜索树与双向链表
+
+
+//递归调用 左 根 右 遍历
+public class Solution {
+     //双向链表的左边头结点和右边头节点
+    TreeNode leftHead = null;
+    TreeNode rightHead = null;
+    public TreeNode Convert(TreeNode pRootOfTree) {
+         //递归调用叶子节点的左右节点返回null
+        if(pRootOfTree==null) return null;
+          //第一次运行时，它会使最左边叶子节点为链表第一个节点
+        Convert(pRootOfTree.left);
+        if(rightHead==null){
+            leftHead= rightHead = pRootOfTree;
+        }else{
+            //把根节点插入到双向链表右边，rightHead向后移动
+           rightHead.right = pRootOfTree;
+           pRootOfTree.left = rightHead;
+           rightHead = pRootOfTree;
+        }
+         //把右叶子节点也插入到双向链表（rightHead已确定，直接插入）
+        Convert(pRootOfTree.right);
+         //返回左边头结点
+        return leftHead;
+    }
+}
