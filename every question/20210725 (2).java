70dfc9b26faf9419142adb9dfc169d1781ae3a32
guyihan-zb <2092332348@qq.com>
2021-08-19 23:33:30 +0800

    数组中出现次数超过一半的数字

---
+++ every question/20210725 (2).java
@@ -0,0 +1,21 @@
+//数组中出现次数超过一半的数字
+import java.util.Arrays;
+ 
+public class Solution {
+    public int MoreThanHalfNum_Solution(int [] array) {
+        Arrays.sort(array);
+        int count=0;
+         
+        for(int i=0;i<array.length;i++){
+            if(array[i]==array[array.length/2]){
+                count++;
+            }
+        }
+        if(count>array.length/2){
+            return array[array.length/2];
+        }else{
+            return 0;
+        }
+         
+    }
+}
