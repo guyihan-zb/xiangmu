62b12608c65f8634ee4907e3d7527e65f84d039b
guyihan-zb <2092332348@qq.com>
2021-08-20 23:23:53 +0800

    数组中出现次数超过一半的数字

--- every question/20210725 (2).java
+++ every question/20210725 (2).java
@@ -19,3 +19,30 @@ public class Solution {
          
     }
 }
+
+
+//hashmap
+public class Solution {
+    public int MoreThanHalfNum_Solution(int [] array) {
+    	HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        
+        for(int i=0;i<array.length;i++){
+            
+            if(!map.containsKey(array[i])){
+               map.put(array[i],1); 
+            }else{
+                int count = map.get(array[i]);
+                map.put(array[i],++count);
+            }
+        }
+        Iterator iter = map.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry entry = (Map.Entry)iter.next();
+            Integer key =(Integer)entry.getKey();
+            Integer val = (Integer)entry.getValue();
+            if(val>array.length/2){
+                return key;
+            }
+        }
+        return 0;
+}
