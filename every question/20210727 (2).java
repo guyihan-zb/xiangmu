2c6a537838f27553dcc47bd8a405fcff21c034aa
guyihan-zb <2092332348@qq.com>
2021-08-29 00:07:34 +0800

    两个链表的第一个公共节点

---
+++ every question/20210727 (2).java
@@ -0,0 +1,15 @@
+// 两个链表的第一个公共节点
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
