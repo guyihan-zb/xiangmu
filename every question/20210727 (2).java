8eb49eafbc98c33fb58ae395a32701e1da2e2018
guyihan-zb <2092332348@qq.com>
2021-08-30 22:49:53 +0800

    两个链表的第一个公共节点

--- every question/20210727 (2).java
+++ every question/20210727 (2).java
@@ -13,3 +13,22 @@ public class Solution {
         return pA;
     }
 }
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
