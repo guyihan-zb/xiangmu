899e3ccc70c059d2baac54904c84c96a0707c55b
guyihan-zb <2092332348@qq.com>
2021-09-02 22:45:59 +0800

    数字在升序数组中出现的次数

--- every question/20210728.java
+++ every question/20210728.java
@@ -19,3 +19,28 @@ public class Solution {
         }
     }
 }
+
+二分法 找到k的位置mid，然后k必然在left和right之间，遍历即可
+
+public class Solution {
+    public int GetNumberOfK(int [] array , int k) {
+       if(array==null||array.length==0) return 0;
+        int left=0,right=array.length-1;
+        int mid=0,cnt=0;
+        while(left<right){
+            mid=left+(right-left)/2;
+            if(array[mid]>k)
+                right=mid-1;
+            else if(array[mid]<k)
+                left=mid+1;
+            else{
+                break;
+            }
+        }
+        for(int i=left;i<=right;i++)
+            if(array[i]==k)
+                cnt++;
+        return cnt;
+
+    }
+}
