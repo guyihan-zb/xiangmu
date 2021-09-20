59522552388c56378fc08a60439df96ea24f6f7b
guyihan-zb <2092332348@qq.com>
2021-09-20 23:37:38 +0800

    翻转单词序列 扑克牌顺子

--- every question/20210801.java
+++ every question/20210801.java
@@ -15,3 +15,34 @@ public class Solution {
         return sb+"";
     }
 }
+
+
+//扑克牌顺子
+
+import java.util.*;
+
+public class Solution {
+    public boolean IsContinuous(int [] numbers) {
+        int mini = 100;
+        int max = 0;
+        int countzero = 0;
+        boolean flag = false;
+        for(int i = 0;i<numbers.length;i++){
+            if(numbers[i] == 0){
+                countzero++;
+                continue;
+            }
+            if(numbers[i] < mini){
+                mini = numbers[i];
+            }
+            if(numbers[i] > max){
+                max = numbers[i];
+            }
+        }
+        if(max-mini+countzero == 4 || max-mini == 4){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
